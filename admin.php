1c0d025dc6962d29a80c24a8267827d672820a8a
roccomilicic <roccomilicic2@gmail.com>
2024-05-19 19:41:43 +1200

    Create PHP file and connect to PHP my admin DB

---
+++ admin.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "webdev.aut.ac.nz";
+$username = "khf9116";
+$password = "nrnlbxrmrquzonykwbjeqcyfoasrzugw";
+$dbname = "khf9116";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    $response = array("success" => false, "error" => "Connection failed: " . $conn->connect_error);
+    echo json_encode($response);
+    exit();
+}
+?>
